6a0594318c084ce5021e499627adf14d2fc815a8
sh1nn0on3 <sanogaku31@gmail.com>
2023-03-02 01:53:45 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 
 import Header from "./components/header/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
